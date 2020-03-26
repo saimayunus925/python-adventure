0060018c376170ff6a9b23fb6ee42c97ba1df814
Saima Yunus <saimayunus925@gmail.com>
2020-03-25 23:57:20 -0700

    fixed up class a little bit, made a generic add() function to add powers and skills, skills list now a dict too

--- game.py
+++ game.py
@@ -10,15 +10,28 @@
 class Character:
     # class attribute, doesn't change depending on instances
     health = 10000
-    def __init__(self, atk, traits=[], powers_map={}, skills=[]):
+    def __init__(self, traits=[], powers_map={}, skills_map={}):
         self.traits = traits 
         self.powers_map = powers_map
-        self.atk = atk
-        self.skills = skills
+        self.skills_map = skills_map
 
     # so 'powers_map' is a dictionary
-    # the keys are the power names themselves, i.e. 'flight', 'super intelligence', 
+    # the keys are the power names themselves, i.e. 'flight', 'super strength', 
+    # 'shapeshifting', etc.
+    # the values are the power attack points/strength ('flight' usually has 0 atk pts, 'super strength' has 
+    # maybe 75 atk points)
+    # same for skills_map, only skills_map is for skills, i.e. 'hand-to-hand combat', 'archery', etc.
     
     # appending a trait or power or skill to the existing list to use
+    # using a generic function for dictionaries since both powers_map
+    # and skills_map are dicts
+    def add(self, str, num):
+        n = int(input("1 - Add New Power, 2 - Add New Skill. Any Other Key? Exit")) 
+        if n == 1:
+            self.powers_map[str] = num
+        elif n == 2:
+            self.skills_map[str] = num
+        else:
+            exit(1) 
 
 
